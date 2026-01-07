798eb317de3dde83a687be142c6cfec3af4b0116
Wille Norberg <wille.norberg.4169@student.uu.se>
2026-01-07 20:31:40 +0100

    Add basic activity logging form

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const PORT = 3000;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+
+
 app.get("/", (req, res)=>{
     res.render("home");
 });
 
+app.post("/log", (req, res) =>{
+    console.log(req.body.activity);
+    res.redirect("/")
+})
+
+
+/* SERVER */
 app.listen(PORT, ()=>{
     console.log(`Servers is listening on http://localhost:${PORT}`)
 });
