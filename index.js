02c66b364f4a5c31b61faff1c9ae5b7dbe44374b
Wille Norberg <wille.norberg.4169@student.uu.se>
2026-01-08 20:52:40 +0100

    Building, some error to show newUser idk

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const Database = require("better-sqlite3")
+const db = new Database("database.db");
+db.pragma("foreign_keys = ON")
+
+
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +14,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
 app.get("/", (req, res)=>{
-    res.render("home", {text: "World"});
+    res.render("home");
 });
 
 
@@ -18,20 +23,24 @@ app.post("/log", (req, res) =>{
     res.redirect("/")
 })
 
+app.post("/signup", (req, res)=>{
+    res.render("/newUser")
+})
+
 const userRouter = require("./routes/users")
 
 
 app.use("/users", userRouter)
 
 
-function logger(req, res, next){
-    console.log(req.originalUrl)
-    next()
-}
+// function logger(req, res, next){
+//     console.log(req.originalUrl)
+//     next()
+// }
 
 /* SERVER */
 app.listen(PORT, ()=>{
     console.log(`Servers is listening on http://localhost:${PORT}`)
 });
 
-//Rep uloaded to Render that deploys it
+//Repoistory uploaded to Render that deploys it
--- routes/users.js
+++ routes/users.js
@@ -6,38 +6,38 @@ router.get("/", (req, res)=>{
     res.send("User List")
 });
 
-router.get("/new", (req, res)=>{
-    res.render("users/new", {firstName: "Enter name"});
-});
+// router.get("/new", (req, res)=>{
+//     res.render("users/new", {firstName: "Enter name"});
+// });
 
-router.post("/", (req, res, next)=>{
-    const isValid = false
-    if (isValid){
-        users.push({firstName: req.body.firstName})
-        res.redirect(`/users/${users.length - 1}`)
-    }
-    else{
-        console.log("Error")
-        res.render("users/new", {firstName: req.body.firstName})
-    }
-})
-
-
-router.route("/:userId")
-    .get((req, res)=>{
-        console.log(req.user)
-        res.send(`Get User With ID ${req.params.userId}`)
-    }).put((req, res)=>{
-        res.send(`Update User With ID ${req.params.userId}`)
-    }).delete((req, res)=>{
-        res.send(`Delete USer With ID ${req.params.userId}`)
-    })
+// router.post("/", (req, res, next)=>{
+//     const isValid = false
+//     if (isValid){
+//         users.push({firstName: req.body.firstName})
+//         res.redirect(`/users/${users.length - 1}`)
+//     }
+//     else{
+//         console.log("Error")
+//         res.render("users/new", {firstName: req.body.firstName})
+//     }
+// })
 
 
-const users = [{name: "Bob"}, {name: "Ricky"}]
-router.param("userId", (req, res, next, userId) =>{
-    req.user = users[userId]
-    next()
-})
+// router.route("/:userId")
+//     .get((req, res)=>{
+//         console.log(req.user)
+//         res.send(`Get User With ID ${req.params.userId}`)
+//     }).put((req, res)=>{
+//         res.send(`Update User With ID ${req.params.userId}`)
+//     }).delete((req, res)=>{
+//         res.send(`Delete USer With ID ${req.params.userId}`)
+//     })
+
+
+// const users = [{name: "Bob"}, {name: "Ricky"}]
+// router.param("userId", (req, res, next, userId) =>{
+//     req.user = users[userId]
+//     next()
+// })
 
 module.exports = router
